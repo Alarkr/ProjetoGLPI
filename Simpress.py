e91bac7599601f400e92690ec77e4a79ae4bd18d
Paulo Alarcão <alarcaorpaulo@gmail.com>
2022-10-27 17:10:36 -0300

    Atualização 27/10

--- Simpress.py
+++ Simpress.py
@@ -9,16 +9,19 @@ chrome_options.headless = True
 driver = webdriver.Chrome(options=chrome_options)'''
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get('http://10.16.51.65/sws/index.html')
-sleep(3)
+sleep(2)
 driver.find_element(By.XPATH, '//*[@id="ext-gen249"]').click()
 sleep(2)
 driver.find_element(By.XPATH, '//*[@id="ext-gen150"]/div/li/ul/li[3]/div/a/span').click()
-modelo = driver.find_element(By.XPATH, '//*[@id="ext-gen827"]')
-contador = driver.find_element(By.XPATH, '//*[@id="ext-gen849"]/div[3]/table/tbody/tr/td[6]/div')
+modelo = driver.find_element(By.XPATH, '//*[@id="ext-gen1053"]')
+contador = driver.find_element(By.XPATH, '//*[@id="ext-gen1075"]/div[3]/table/tbody/tr/td[6]/div')
 
 print(f'Modelo {modelo}\n'
       f'Contador {contador}')
+
+
 
 
 
--- testes.py
+++ testes.py
@@ -1,4 +1,3 @@
-import unittest
 import pyautogui
 import requests
 from selenium import webdriver
@@ -6,54 +5,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
 
-
-def usuarios():
-    sleep(0.4)
-    navegador.find_element(By.XPATH, '//*[@id="select2-chosen-6"]').click()
-    sleep(0.4)
-    navegador.find_element(By.XPATH, '//*[@id="s2id_autogen6_search"]').send_keys('carlos.sousa')
-    sleep(0.4)
-    navegador.find_element(By.XPATH, '//*[@id="s2id_autogen6_search"]').send_keys(Keys.ENTER)
-    sleep(0.4)
-    navegador.find_element(By.XPATH, '//*[@id="select2-chosen-24"]').click()
-    print('=' * 20)
-    print('Requerente Adicionado')
-
-
 navegador = webdriver.Chrome()  # Abre o Navegador
-navegador.get('http://glpi/glpi/front/ticket.php')  # Abre o GLPI
-navegador.find_element('xpath', '//*[@id="login_name"]').send_keys('paulo.alarcao')  # Coloca usuario
-navegador.find_element('xpath', '//*[@id="login_password"]').send_keys('Paulo@123452w')  # Coloca senha
-navegador.find_element('xpath', '//*[@id="boxlogin"]/form/p[3]/input').send_keys(Keys.ENTER)  # Clica em enter para acessar
-navegador.find_element('xpath', '//*[@id="menu_all_button"]').send_keys(Keys.ENTER)  # Abre o Menu
-navegador.find_element('xpath', '//*[@id="show_all_menu"]/table[2]/tbody/tr[3]/td/a').send_keys(Keys.ENTER)  # Clica para abrir chamados
-sleep(0.4)
-navegador.find_element(By.XPATH, '//*[@id="select2-chosen-6"]').click()
-sleep(0.4)
-navegador.find_element(By.XPATH, '//*[@id="s2id_autogen6_search"]').send_keys('carlos.sousa')
-sleep(0.4)
-navegador.find_element(By.XPATH, '//*[@id="s2id_autogen6_search"]').send_keys(Keys.ENTER)
-sleep(0.4)
-navegador.find_element(By.CLASS_NAME, 'select2-hidden-accessible').click()
-
-
-
-
-
-
-'''navegador = webdriver.Chrome()  # Abre o Navegador
 navegador.get('http://glpi/glpi/front/ticket.php')  # Abre o GLPI
 sleep(1)
 navegador.find_element('xpath', '//*[@id="login_name"]').send_keys('paulo.alarcao')  # Coloca usuario
 sleep(1)
 navegador.find_element('xpath', '//*[@id="login_password"]').send_keys('Paulo@123452w', Keys.ENTER)  # Coloca senha
 sleep(1)
-navegador.get('http://glpi/glpi/front/ticket.form.php?id=95429')
+navegador.get('http://glpi/glpi/front/ticket.form.php?id=95517')
 sleep(1)
 navegador.find_element(By.XPATH, '//*[@id="ui-id-5"]').click()
 sleep(1)
 navegador.find_element(By.XPATH, '//*[@id="ui-tabs-2"]/div/div[1]/ul/li[4]').click()
 sleep(1)
 navegador.find_element(By.ID, '//*[@id="tinymce"]').click()
-navegador.find_element(By.XPATH, '//*[@id="tinymce"]').send_keys('teste')
-sleep(10)'''
+navegador.find_element(By.XPATH, 'http://glpi/glpi/front/ticket.form.php?id=95517').send_keys('teste')
+sleep(10)
